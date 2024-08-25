1ef44ead26840a83180aacccf2df302f31d8eb9b
Ejaz0000 <ejahmed999@gmail.com>
2024-08-25 16:12:03 +0600

    Fetch your data from server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Form from './component/Form';
+import Products from './component/Products';
 
 function App() {
   return (
     <div className='main'>
       <Form/>
+      <Products/>
     </div>
   );
 }
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './products.css'
 
 const Products = () => {
+
+    const [products,setProducts] =useState([]);
+
+
+    useEffect(()=>{
+        async function fetchProducts(){
+            try{
+                const res = await fetch("https://react-interview.1putym.easypanel.host/api/product?email=naim.microdeft@gmail.com")
+
+                if(!res.ok){
+                    throw new Error("data fetch failed");
+                }
+
+                const data = await res.json();
+                console.log(data.data);
+
+                
+
+                setProducts(data.data)
+                
+            }catch(error){
+                console.log(error);
+            }
+
+
+        }
+
+        fetchProducts()
+    },[])
+
+
   return (
-    <div>Products</div>
+    <div className='w-full box-border p-6 flex flex-wrap justify-center gap-2'>
+
+        {products.length > 0 && products.map(product =>
+                <div className='card' key={product.id}>
+                    <div className='prod_img'>
+                      <img src={product.image} alt='prod'/>
+                    </div>
+
+                    <p className='prod_name'>{product.title}</p>
+                    <p>${product.price}</p>
+                    <button>Order Now</button>
+                </div>
+
+            )}
+
+    </div>
   )
 }
 
